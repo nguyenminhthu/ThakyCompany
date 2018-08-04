495357da5869c18530478cb05dee76629e370579
minhthu2511 <minhthu2511@gmail.com>
2018-08-04 14:59:20 +0700

    Project Form mã hóa

---
+++ EncryptText/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace EncryptText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTextToEncrypt = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.txtEncryptResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDescrypt = new System.Windows.Forms.Button();
+            this.txtSecurityKey = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text to encrypt";
+            // 
+            // txtTextToEncrypt
+            // 
+            this.txtTextToEncrypt.Location = new System.Drawing.Point(82, 13);
+            this.txtTextToEncrypt.Name = "txtTextToEncrypt";
+            this.txtTextToEncrypt.Size = new System.Drawing.Size(805, 20);
+            this.txtTextToEncrypt.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(812, 39);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtEncryptResult
+            // 
+            this.txtEncryptResult.Location = new System.Drawing.Point(82, 94);
+            this.txtEncryptResult.Multiline = true;
+            this.txtEncryptResult.Name = "txtEncryptResult";
+            this.txtEncryptResult.Size = new System.Drawing.Size(805, 56);
+            this.txtEncryptResult.TabIndex = 3;
+            this.txtEncryptResult.Click += new System.EventHandler(this.txtEncryptResult_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-2, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Encrypt result";
+            // 
+            // btnDescrypt
+            // 
+            this.btnDescrypt.Location = new System.Drawing.Point(731, 39);
+            this.btnDescrypt.Name = "btnDescrypt";
+            this.btnDescrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDescrypt.TabIndex = 5;
+            this.btnDescrypt.Text = "Descrypt";
+            this.btnDescrypt.UseVisualStyleBackColor = true;
+            this.btnDescrypt.Click += new System.EventHandler(this.btnDescrypt_Click);
+            // 
+            // txtSecurityKey
+            // 
+            this.txtSecurityKey.Location = new System.Drawing.Point(82, 42);
+            this.txtSecurityKey.Name = "txtSecurityKey";
+            this.txtSecurityKey.Size = new System.Drawing.Size(337, 20);
+            this.txtSecurityKey.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-1, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Security Key";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(382, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 186);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSecurityKey);
+            this.Controls.Add(this.btnDescrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEncryptResult);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtTextToEncrypt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTextToEncrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox txtEncryptResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDescrypt;
+        private System.Windows.Forms.TextBox txtSecurityKey;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
